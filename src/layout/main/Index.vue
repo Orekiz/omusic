7d5400fb88a5b42374fd180a0719f029ee9644c2
Oreki <35621834+Orekiz@users.noreply.github.com>
2022-08-31 22:09:04 +0800

    perf: 三大主路由keep-alive

--- src/layout/main/Index.vue
+++ src/layout/main/Index.vue
@@ -3,7 +3,11 @@
 
 <template>
 <main>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
 </main>
 </template>
 
