46353838acbc4684e08a9cd9bb170e82ceec5f88
Oreki <35621834+Orekiz@users.noreply.github.com>
2022-09-29 12:19:22 +0800

    fix: 添加拦截器，解决api返回状态码的问题。

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -21,13 +21,17 @@ class Request {
       .then(res => {
         // 200: 请求成功
         if (res.status === 200) {
-          resolve(res.data)
+          if (res.data.code === 200) {
+            resolve(res.data)
+          } else {
+            reject(res.data)
+          }
         } else {
-          resolve({ code: 201, message: res.data.message ?? '请求失败', data: null })
+          reject({ code: 201, message: res.data.message ?? '请求失败', data: null })
         }
       })
       .catch(err => {
-        const myMessage = err?.data?.message ?? err?.message ?? '请求失败'
+        const myMessage = err?.data?.message ?? err?.message ?? err?.msg ?? '请求失败'
         reject(new Error(myMessage))
       })
     })
@@ -37,15 +41,20 @@ class Request {
     return new Promise((resolve, reject) => {
       instance.post(`${this.baseUrl}${url}`, { ...data, realIP: '139.196.215.163', timestamp: getTimestamp() })
       .then(res => {
+        console.log(res)
         // 200: 请求成功
         if (res.status === 200) {
-          resolve(res.data)
+          if (res.data.code === 200) {
+            resolve(res.data)
+          } else {
+            reject(res.data)
+          }
         } else {
-          resolve({ code: 201, message: res.data.message ?? '请求失败', data: null })
+          reject({ code: 201, message: res.data.message ?? '请求失败', data: null })
         }
       })
       .catch(err => {
-        const myMessage = err?.data?.message ?? err?.message ?? '请求失败'
+        const myMessage = err?.data?.message ?? err?.message ?? err?.msg ?? '请求失败'
         reject(new Error(myMessage))
       })
     })
